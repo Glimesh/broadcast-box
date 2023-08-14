51c0655751995ab790bd6778ccc4a2a855f49916
makindotcc <9150636+makindotcc@users.noreply.github.com>
2023-08-15 01:28:34 +0200

    Remove duplicated home link in navbar
    It is common UX that logo redirects to home page. Also browser has history stack already.

--- web/src/components/header/index.js
+++ web/src/components/header/index.js
@@ -12,21 +12,9 @@ const Header = () => {
         <nav className='bg-gray-800 p-2 mt-0 fixed w-full z-10 top-0'>
           <div className='container mx-auto flex flex-wrap items-center'>
             <div className='flex flex-1 text-white font-extrabold'>
-              <a href="/" className='font-light leading-tight text-2xl'>
+              <Link to="/" className='font-light leading-tight text-2xl'>
                 Broadcast Box
-              </a>
-            </div>
-            <div className='flex content-center justify-between md:w-1/2 md:justify-end'>
-              <ul className='list-reset flex justify-between flex-1 md:flex-none items-center'>
-                <li className=''>
-                  <button
-                    className='py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75'
-                    onClick={() => { navigate('/') }}
-                  >
-                    Go Home
-                  </button>
-                </li>
-              </ul>
+              </Link>
             </div>
           </div>
         </nav>
