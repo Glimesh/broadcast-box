7cdd7122d787e790eabb87fe0768a0e822411c97
Christopher Martin <cgmartin@gmail.com>
2024-05-30 20:56:35 -0400

    Change cinema mode state to use query search params instead of localstorage

--- web/src/index.js
+++ web/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { CinemaModeProvider } from './components/player'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <CinemaModeProvider>
-      <App />
-    </CinemaModeProvider>
+    <Router>
+      <CinemaModeProvider>
+        <App />
+      </CinemaModeProvider>
+    </Router>
   </React.StrictMode>
 )
