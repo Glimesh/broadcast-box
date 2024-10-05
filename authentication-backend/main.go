ccca6211d8f46e69911cc9f20fca029e60cf0c5a
Neil Schark <gitlab@schark.eu>
2024-10-05 20:19:57 +0000

    init auth with pocketbase

--- authentication-backend/main.go
+++ authentication-backend/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/core"
+)
+
+func main() {
+	app := pocketbase.New()
+
+	// serves static files from the provided public dir (if exists)
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
+		return nil
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
