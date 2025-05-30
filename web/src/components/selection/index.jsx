b9b40952b83421f98aa73c10480aa1d1a9c6666b
Kim M. Larsen <kim.m.larsen@outlook.com>
2025-05-30 23:42:50 +0200

    Use correct method for stream link.

--- web/src/components/selection/index.jsx
+++ web/src/components/selection/index.jsx
@@ -95,7 +95,7 @@ function Selection() {
           <button
             key={i+'_'+e.key}
             className={`mt-2 py-2 px-4 ${e.isActive ? 'bg-blue-500' : 'bg-orange-500'} text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-hidden focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75`}
-            onClick={() => onPublishStreamClick(e.key)}>
+            onClick={() => onWatchStreamClick(e.key)}>
             {e.key}
           </button>))
         }
