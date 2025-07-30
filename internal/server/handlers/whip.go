e7c12b8fc8205aa92b13b5e1c494feab1061096e
Mal <kim.m.larsen@outlook.com>
2025-07-30 17:35:11 +0200

    Implement Whip API

---
+++ internal/server/handlers/whip.go
@@ -0,0 +1,94 @@
+package handlers
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/glimesh/broadcast-box/internal/server/authorization"
+	"github.com/glimesh/broadcast-box/internal/server/helpers"
+	"github.com/glimesh/broadcast-box/internal/server/webhook"
+	"github.com/glimesh/broadcast-box/internal/webrtc"
+)
+
+func whipHandler(responseWriter http.ResponseWriter, request *http.Request) {
+	if request.Method == "DELETE" {
+		return
+	}
+
+	authHeader := request.Header.Get("Authorization")
+
+	if authHeader == "" {
+		log.Println("Authorization was not set")
+		helpers.LogHttpError(responseWriter, "Authorization was not set", http.StatusBadRequest)
+	}
+
+	token := helpers.ResolveBearerToken(authHeader)
+	if token == "" {
+		log.Println("Authorization was invalid")
+		helpers.LogHttpError(responseWriter, "Authorization was invalid", http.StatusUnauthorized)
+		return
+	}
+
+	offer, err := io.ReadAll(request.Body)
+	if err != nil {
+		log.Println(err.Error())
+		helpers.LogHttpError(responseWriter, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var userProfile authorization.Profile
+
+	// Stream requires webhook validation
+	if webhookUrl := os.Getenv("WEBHOOK_URL"); webhookUrl != "" {
+		streamKey, err := webhook.CallWebhook(webhookUrl, webhook.WhipConnect, token, request)
+		if err != nil {
+			responseWriter.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		userProfile = authorization.Profile{
+			StreamKey: streamKey,
+			IsPublic:  true,
+			MOTD:      "Welcome to " + token + "'s stream!",
+		}
+	}
+
+	// Stream requires profile
+	if requiresStreamProfile := os.Getenv("STREAM_PROFILE_ACTIVE"); requiresStreamProfile != "" {
+		profile, err := authorization.GetProfile(token)
+		if err != nil {
+			log.Println("Unauthorized login attempt with bearer", token)
+			responseWriter.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		userProfile = *profile
+	}
+
+	if userProfile == (authorization.Profile{}) {
+		userProfile = authorization.Profile{
+			StreamKey: token,
+			IsPublic:  true,
+			MOTD:      "Welcome to " + token + "'s stream!",
+		}
+	}
+
+	whipAnswer, err := webrtc.WHIP(string(offer), userProfile)
+	if err != nil {
+		log.Println("WHIP Error", err.Error())
+		helpers.LogHttpError(responseWriter, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	responseWriter.Header().Add("Location", "/api/whip")
+	responseWriter.Header().Add("Content-Type", "application/sdp")
+	responseWriter.WriteHeader(http.StatusCreated)
+
+	if _, err = fmt.Fprint(responseWriter, whipAnswer); err != nil {
+		log.Println("Error", err)
+	} else {
+		log.Println("API.WHIP Completed")
+	}
+}
