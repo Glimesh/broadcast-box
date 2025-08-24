f0a6e90c4ddd6a9d778f9ceaf8f728c52363d406
Mal <kim.m.larsen@outlook.com>
2025-08-24 12:11:06 +0200

    Add IsDisabled property to Button component

--- web/src/components/shared/Button.tsx
+++ web/src/components/shared/Button.tsx
@@ -2,14 +2,15 @@ import React from "react";
 
 interface Props {
   title?: string;
+  isDisabled?: boolean;
   onClick?: () => void;
 }
 
 export default function Button(props: Props) {
   return (
     <button
-      className="bg-green-700 text-white px-4 py-2 rounded w-full"
-      onClick={props.onClick}
+      className={`text-white px-4 py-2 rounded w-full ${props.isDisabled ? "bg-gray-600" : "hover:bg-green-500   bg-green-700"}`}
+      onClick={!props.isDisabled ? props.onClick : undefined}
     >
       {props.title}
     </button>
