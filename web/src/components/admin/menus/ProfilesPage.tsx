482bca983ddd51245c596ec6a6ce38fff4e0a4bc
Mal <kim.m.larsen@outlook.com>
2025-08-24 12:12:48 +0200

    Add profile features for overview, add, remove and refresh token

--- web/src/components/admin/menus/ProfilesPage.tsx
+++ web/src/components/admin/menus/ProfilesPage.tsx
@@ -1,5 +1,8 @@
-import { ArrowPathIcon } from "@heroicons/react/16/solid";
+import { ArrowPathIcon, XMarkIcon } from "@heroicons/react/16/solid";
 import React, { useEffect, useState } from "react";
+import Button from "../../shared/Button";
+import ModalTextInput from "../../shared/ModalTextInput";
+import ModalMessageBox from "../../shared/ModalMessageBox";
 
 const ADMIN_TOKEN = "adminToken";
 
@@ -12,6 +15,9 @@ interface Profile {
 
 const ProfilesPage = () => {
   const [response, setResponse] = useState<Profile[]>()
+  const [isAddProfileModalOpen, setIsAddProfileModalOpen] = useState<boolean>(false)
+  const [isRemoveProfileModalOpen, setIsRemoveProfileModalOpen] = useState<string>("")
+  const [errorMessage, setErrorMessage] = useState<string>()
 
   const refreshProfiles = () => {
     fetch(`/api/admin/profiles`, {
@@ -19,35 +25,80 @@ const ProfilesPage = () => {
       headers: {
         Authorization: `Bearer ${localStorage.getItem(ADMIN_TOKEN)}`,
       },
-    })
-      .then((result) => {
-        if (result.status >= 400 && result.status < 500) {
-          localStorage.removeItem(ADMIN_TOKEN)
-          return;
-        }
+    }).then((result) => {
+      if (result.status > 400 && result.status < 500) {
+        localStorage.removeItem(ADMIN_TOKEN)
+        return;
+      }
 
-        return result.json();
-      })
-      .then((result) => {
-        setResponse(() => result)
-      });
+      return result.json();
+    }).then((result) => {
+      setResponse(() => result)
+    });
   };
-  const resetToken = (streamKey: string) => {
+  const resetProfileToken = (streamKey: string) => {
     fetch(`/api/admin/profiles/reset-token`, {
       method: "POST",
       headers: {
         Authorization: `Bearer ${localStorage.getItem(ADMIN_TOKEN)}`,
       },
       body: JSON.stringify({ streamKey: streamKey })
-    })
-      .then((result) => {
-        if (result.status >= 400 && result.status < 500) {
-          localStorage.removeItem(ADMIN_TOKEN)
-          return;
-        }
+    }).then((result) => {
+      if (result.status > 400 && result.status < 500) {
+        localStorage.removeItem(ADMIN_TOKEN)
+        return;
+      }
 
-        refreshProfiles()
-      });
+      refreshProfiles()
+    });
+  };
+  const addProfile = (streamKey: string) => {
+    fetch(`/api/admin/profiles/add-profile`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem(ADMIN_TOKEN)}`,
+      },
+      body: JSON.stringify({ streamKey: streamKey })
+    }).then((result) => {
+      if (result.status > 400 && result.status < 500) {
+        localStorage.removeItem(ADMIN_TOKEN)
+        return;
+      }
+
+      if (result.status === 400) {
+        result.text()
+          .then((resultText) => setErrorMessage(() => resultText))
+
+        return
+      }
+
+      setIsAddProfileModalOpen(() => false)
+      refreshProfiles()
+    });
+  };
+  const removeProfile = (streamKey: string) => {
+    fetch(`/api/admin/profiles/remove-profile`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem(ADMIN_TOKEN)}`,
+      },
+      body: JSON.stringify({ streamKey: streamKey })
+    }).then((result) => {
+      if (result.status > 400 && result.status < 500) {
+        localStorage.removeItem(ADMIN_TOKEN)
+        return;
+      }
+
+      if (result.status === 400) {
+        result.text()
+          .then((resultText) => setErrorMessage(() => resultText))
+
+        return
+      }
+
+      setIsRemoveProfileModalOpen(() => "")
+      refreshProfiles()
+    });
   };
 
   useEffect(() => {
@@ -55,10 +106,31 @@ const ProfilesPage = () => {
   }, [])
 
   return (
-    <div className="p-6 w-full max-w-6xl mx-auto">
+    <div className="p-6 w-full h-full max-w-6xl mx-auto flex flex-col justify-between">
       <h1 className="text-3xl font-bold mb-6">Profiles Overview</h1>
 
-      <div className="overflow-x-auto">
+      <div className="overflow-x-auto h-full">
+        <ModalTextInput
+          title="Add Profile"
+          message="Insert a key to add a new stream"
+          errorMessage={errorMessage}
+          placeholder="Write new stream key here"
+          isOpen={isAddProfileModalOpen}
+          canCloseOnBackgroundClick={true}
+          onAccept={(result) => addProfile(result.toString())}
+          onDeny={() => setIsAddProfileModalOpen(false)}
+        />
+
+        <ModalMessageBox
+          title="Remove profile"
+          message={`Are you sure you would like to remove ${isRemoveProfileModalOpen}`}
+          errorMessage={errorMessage}
+          isOpen={isRemoveProfileModalOpen !== ""}
+          canCloseOnBackgroundClick={true}
+          onAccept={() => removeProfile(isRemoveProfileModalOpen)}
+          onDeny={() => setIsRemoveProfileModalOpen("")}
+        />
+
         <table className="min-w-full rounded-lg">
           <thead className=" text-white">
             <tr>
@@ -66,6 +138,7 @@ const ProfilesPage = () => {
               <th className="px-4 py-2 text-left">Is Public</th>
               <th className="px-4 py-2 text-left">Motd</th>
               <th className="px-4 py-2 text-left">Token</th>
+              <th className="px-4 py-2 text-left"></th>
             </tr>
           </thead>
           <tbody>
@@ -78,7 +151,15 @@ const ProfilesPage = () => {
                   <td className="px-4 py-2 flex flex-row justify-between" title="To be implemented">
                     {profile.token}
 
-                    <ArrowPathIcon onClick={() => resetToken(profile.streamKey)} className="ml-2 h-6" />
+                    <ArrowPathIcon
+                      className="ml-2 h-6"
+                      onClick={() => resetProfileToken(profile.streamKey)} />
+                  </td>
+                  <td className="px-4 py-2">
+                    <XMarkIcon
+                      className="ml-2 h-6 text-red-700"
+                      onClick={() => setIsRemoveProfileModalOpen(() => profile.streamKey)}
+                    />
                   </td>
                 </tr>
               );
@@ -86,8 +167,12 @@ const ProfilesPage = () => {
           </tbody>
         </table>
       </div>
+
+      <Button
+        title="Add profile"
+        onClick={() => setIsAddProfileModalOpen(() => true)}
+      />
     </div>
   );
 }
 export default ProfilesPage;
-
