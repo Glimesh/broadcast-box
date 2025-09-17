1a8cd2c1ceab95f254a739a63012e6b9a59f5c7b
Mal <kim.m.larsen@outlook.com>
2025-09-17 23:30:11 +0200

    Add Copy icon

--- web/src/components/shared/Icons.tsx
+++ web/src/components/shared/Icons.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-export type IconType = "People" | "Camera" | "SquaresPlus" | "CodeBracketSquare" | "Language"
+export type IconType = "People" | "Camera" | "SquaresPlus" | "CodeBracketSquare" | "Language" | "Copy"
 
 export const getIcon = (icon: IconType) => {
   return icon === "People" ? peopleIcon
@@ -8,7 +8,8 @@ export const getIcon = (icon: IconType) => {
       : icon === "SquaresPlus" ? squaresPlus
         : icon === "CodeBracketSquare" ? codeBracketSquare
           : icon === "Language" ? language
-            : ""
+            : icon === "Copy" ? copy
+              : ""
 }
 
 export const peopleIcon =
@@ -34,5 +35,10 @@ export const codeBracketSquare =
 export const language =
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
     <path strokeLinecap="round" strokeLinejoin="round" d="m10.5 21 5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 0 1 6-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 0 1-3.827-5.802" />
+  </svg>
+
+export const copy =
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
   </svg>
 
