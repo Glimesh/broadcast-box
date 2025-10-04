8004573cf95cd185e7b4016004cb988e222ab450
= <kim.m.larsen@outlook.com>
2025-10-04 16:55:50 +0200

    Bug: ASCII strings caught as B64 strings
    Some strings are valid B64 and was handled incorrectly. This adds
    additional validation of B64

--- internal/server/helpers/token.go
+++ internal/server/helpers/token.go
@@ -2,14 +2,20 @@ package helpers
 
 import (
 	"encoding/base64"
+	"fmt"
+	"log"
+	"regexp"
 	"strings"
 )
 
+// Resolve Bearer token.
+// This supports both a B64 token as well as a regular ASCII token to allow for
+// using special characters for stream keys that are not tokens
 func ResolveBearerToken(authHeader string) string {
 	const bearerPrefix = "Bearer "
 	if result, ok := strings.CutPrefix(authHeader, bearerPrefix); ok {
 
-		if base64String, err := getBase64String(strings.ReplaceAll(result, " ", "")); err == nil {
+		if base64String, err := getBase64String(result); err == nil {
 			return strings.ReplaceAll(base64String, " ", "_")
 		}
 
@@ -22,7 +28,14 @@ func ResolveBearerToken(authHeader string) string {
 // In case the bearer token is encoded in Base64, it can be resolved before return. This
 // allows for UTF-8 character support in headers with bearer tokens
 func getBase64String(token string) (result string, err error) {
+	log.Println("Checking B64 for string", token)
+	if !regexp.MustCompile(`^[A-Za-z0-9+/]+={0,2}$`).MatchString(token) {
+		log.Println("Failed B64 for string", token)
+		return "", fmt.Errorf("string is not valid base64")
+	}
+
 	output, err := base64.StdEncoding.DecodeString(token)
+	log.Println("Converting", token, " to B64", string(output))
 	if err != nil {
 		return "", err
 	}
