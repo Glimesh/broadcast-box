0e3d7263c9c635776eb8dbd5b00894eeb32c2921
= <kim.m.larsen@outlook.com>
2025-11-27 23:04:17 +0100

    Only run profiling if set in envar

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"runtime"
 	"strings"
 	"time"
 
@@ -11,12 +12,23 @@ import (
 	"github.com/glimesh/broadcast-box/internal/server"
 	"github.com/glimesh/broadcast-box/internal/test"
 	"github.com/glimesh/broadcast-box/internal/webrtc"
+
+	"net/http"
+	_ "net/http/pprof"
 )
 
 func main() {
 	environment.SetupLogger()
 	environment.LoadEnvironmentVariables()
 	console.HandleConsoleFlags()
+
+	if shouldProfileApplication := os.Getenv(environment.ENABLE_PROFILING); strings.EqualFold(shouldProfileApplication, "true") {
+		go func() {
+			runtime.SetBlockProfileRate(1)
+			runtime.SetMutexProfileFraction(1)
+			log.Println(http.ListenAndServe("localhost:6060", nil))
+		}()
+	}
 
 	log.Println("Booting up Broadcast", time.Now().Format("2006-01-02 15:04:05"))
 	webrtc.Setup()
