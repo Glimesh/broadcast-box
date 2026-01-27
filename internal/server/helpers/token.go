d84d560a9cd26b05c2f3372345bf1cac10f9cc58
Mal <kim.m.larsen@outlook.com>
2026-01-27 20:57:35 +0100

    Improved Bearer token B64 handling

--- internal/server/helpers/token.go
+++ internal/server/helpers/token.go
@@ -5,20 +5,32 @@ import (
 	"fmt"
 	"regexp"
 	"strings"
+	"unicode/utf8"
 )
 
 // Resolve Bearer token.
 // This supports both a B64 token as well as a regular ASCII token to allow for
 // using special characters for stream keys that are not tokens
-func ResolveBearerToken(authHeader string) string {
+func ResolveBearerToken(authHeader string) (result string) {
 	const bearerPrefix = "Bearer "
-	if result, ok := strings.CutPrefix(authHeader, bearerPrefix); ok {
 
-		if base64String, err := getBase64String(result); err == nil {
+	// Cut the prefix
+	if auth, ok := strings.CutPrefix(authHeader, bearerPrefix); ok {
+
+		// Check for valid b64
+		if base64String, err := getBase64String(auth); err == nil {
 			return strings.ReplaceAll(base64String, " ", "_")
-		}
 
-		return strings.ReplaceAll(result, " ", "_")
+			// Invalid, handle as unicode
+		} else {
+			re := regexp.MustCompile(`^[A-Za-z0-9_-]+$`)
+
+			auth = strings.TrimSpace(auth)
+			auth = strings.ReplaceAll(auth, " ", "_")
+			if re.MatchString(auth) {
+				return strings.ReplaceAll(auth, " ", "_")
+			}
+		}
 	}
 
 	return ""
@@ -36,5 +48,9 @@ func getBase64String(token string) (result string, err error) {
 		return "", err
 	}
 
-	return string(output), err
+	if !utf8.Valid(output) {
+		return token, nil
+	}
+
+	return string(output), nil
 }
